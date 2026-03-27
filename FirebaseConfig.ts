9503c9dc9adf92b06d9ab1f9c77cf2431bdc18b3
Osaid-Motawi <s12341143@stu.najah.edu>
2026-03-27 15:37:12 +0300

    finish

---
+++ FirebaseConfig.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCI3ez-S6__mjeEQ3EV8huTPQpp2-5niN0",
+  authDomain: "malath-3f6d8.firebaseapp.com",
+  projectId: "malath-3f6d8",
+  storageBucket: "malath-3f6d8.firebasestorage.app",
+  messagingSenderId: "593418374423",
+  appId: "1:593418374423:web:0a9ec4114989b237f5fb52",
+  measurementId: "G-P4H3ETSHGM"
+};
+
+export const app = initializeApp(firebaseConfig);
